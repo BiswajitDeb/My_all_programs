def1ab8241d26c4d32297684490cef2a52b2b479
Biswajit Deb <biswa2015.bd@gmail.com>
2021-08-02 06:47:46 -0700

    After reinstalling windows 10 pro

--- C/Clear Test/testing.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for(int i=0; i>=0;i++)
-        {printf("Hello");}
-    return 0;
-}
--- C/pratik.c
+++ C/pratik.c
@@ -1,15 +1,8 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main()
 {
-    int arr[1000];
-    for (int i =0;i<19;i++)
-        arr[i]=i+1;
-
-    // int *pr=&arr[13];
-
-    // printf("The address of the element of the array : %d",*pr);
-    printf("Address is at : %d\n",&arr[14]);
-    printf("Address is at : %p",&arr[14]);
-    return 0;
+    int i = 0;
+    for ( ; i++ ; ) 
+       printf("%d",i);
+       return 0;
 }
