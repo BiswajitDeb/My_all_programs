e16ca3c6e6990f2373575df6906514bc7c5faf88
Biswajit Deb <biswa2015.bd@gmail.com>
2021-09-17 16:31:18 +0530

    Assignment 17/09/2021 update

--- C/Third sem/factorial.c
+++ C/Third sem/factorial.c
@@ -4,8 +4,16 @@ int main()
     int n,mul=1;
     printf("Enter a number to print it's factorial : ");
     scanf("%d",&n);
-    for (n;n>=1;n--)
-        mul=mul*n;
+    if(n<0) 
+    {
+        for(n;n<=-1;n++)
+            mul=mul*n;
+    }
+    else 
+        {
+            for (n;n>=1;n--)
+            mul=mul*n;
+        }
     printf("The factorial is : %d",mul);
     return 0;
 }
