1621153709a5e6c1b1260f3b67e8398df6f2eb2b
Biswajit Deb <biswa2015.bd@gmail.com>
2021-09-24 16:37:46 +0530

    Update assignment 6 DSA

--- C/Third sem/fibonacci_range.c
+++ C/Third sem/fibonacci_range.c
@@ -1,12 +1,13 @@
 #include<stdio.h>    
 int main()    
 {    
-    int n1=0,n2=1,temp,i,number;    
+    int n1=0,n2=1,temp,number;    
     printf("Enter the Ending term : ");    
     scanf("%d",&number);    
-    if(number==0)   {printf("0");}
-    if(number==1)   {printf("0 1");} 
-    for(i=2;i<number;++i)
+    printf("0");
+    // if(number==0)   {printf("0");}
+    // if(number==1)   {printf("0 1");} 
+    for(int i=0;i<number;++i)
     {    
         
         temp=n1+n2;
--- C/Third sem/test.c
+++ C/Third sem/test.c
@@ -1,19 +1,19 @@
 #include<stdio.h>    
 int main()    
 {    
-    int n1=0,n2=1,n3,i,number;    
+    int n1=0,n2=1,n3,number,i=2;    
     printf("Enter the Ending term : ");    
     scanf("%d",&number);    
     if(number==0)   {printf("0");}
     if(number==1)   {printf("0 1");} 
-    while(i<number)
+    while(i<=number)
     {    
-        
+        ++i;
         n3=n1+n2;
         //if(n3>=number)    {break;}    
-        printf(" %d",n3);    
         n1=n2;    
         n2=n3;
+        printf(" %d",n3);    
     }  
   return 0;  
 }    
