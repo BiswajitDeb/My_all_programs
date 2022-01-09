0ceea22b9b93833132acf16227031a791ba5c9aa
Biswajit Deb <biswa2015.bd@gmail.com>
2022-01-09 19:34:03 +0530

    Assignment 24 and 25

---
+++ C/Third sem/DSA25_quick_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+void quick_sort(int[], int, int);
+int main()
+{
+    int S, i;
+    printf("Enter array size : ");
+    scanf("%d", &S);
+    int n[100];
+    printf("Enter %d elements in Array : \n", S);
+    for (i = 0; i < S; i++)
+        scanf("%d", &n[i]);
+    quick_sort(n, 0, S - 1);
+    printf("Elements After Sorting: ");
+    for (i = 0; i < S; i++)
+        printf(" %d", n[i]);
+    return 0;
+}
+void quick_sort(int n[], int first, int last)
+{
+    int pivot, j, temp, i;
+    if (first < last)
+    {
+        pivot = first;
+        i = first;
+        j = last;
+        while (i < j)
+        {
+            while (n[i] <= n[pivot] && i < last)
+                i++;
+            while (n[j] > n[pivot])
+                j--;
+            if (i < j)
+            {
+                temp = n[i];
+                n[i] = n[j];
+                n[j] = temp;
+            }
+        }
+        temp = n[pivot];
+        n[pivot] = n[j];
+        n[j] = temp;
+        quick_sort(n, first, j - 1);
+        quick_sort(n, j + 1, last);
+    }
+}
